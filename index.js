01330f53d9caf551e583c4f196fabfccd972d0bd
karthigayani <karthi1826@gmail.com>
2023-09-20 15:15:57 +0530

    organized movies route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
+import moviesRouter from "./routes/movies.route.js"
 dotenv.config();
 
 const app = express();
@@ -29,87 +30,8 @@ app.get("/", function (request, response) {
   response.send("Hello🙋‍♂️, world🌏 🎊✨🤩");
 }); 
 
-app.get("/movies", async function (request, response) {
-  // request.query returns string value.
-  // In our movies collection rating is in number. So if request.query is rating means convert them into number.
-  if(request.query.rating){
-    request.query.rating = +request.query.rating;
-  };
-  console.log(request.query);
-  // db.movies.find({})
-
-  // cursor - pagination when you search in google, it will show top 10 results only. if you want means you go to next next page.
-  // why they don't show all the million results??? why because when you get all the millions of data, browser will strucks to produce results. Also it affects browser database too.
-  // To avoid this browser shows top 10 reults. Likewise mongoDB also shows first 20 results. 
-  
-  // Cursor - pagination | Cursor -> Array | toArray()
-  // const movies = await client.db("recap").collection("movies").find({});
-  // const movies = await client.db("recap").collection("movies").find({}).toArray();
-  
-  // query params: in google or any other search bar, when you seach an item you will can notice (For eg: http link + `?search_query=ps2`). here we are filtering an item.
-  // const movies = await client.db("recap").collection("movies").find({name:"RRR"}).toArray();
-  const movies = await client.db("recap").collection("movies").find(request.query).toArray();
-  // console.log(movies);
-  response.send(movies);
-});
-
-app.get("/movies/:id",async function (request, response) { 
-  const {id} = request.params; 
-  // console.log(request.params, id); 
-  // const movie = movies.find((mv) => mv.id == id); // using local data
-  // getting data from database
-  // db.movies.findOne({id:'100'}) -> db query
-  const movie = await client.db("recap").collection("movies").findOne({id: id});
-
-  console.log(movie);
- 
-  movie ? response.send(movie) : response.status(404).send({message: "movie not found"});
-
-}); 
- 
-app.post("/movies", async function (request, response) {
-  // middleware -> applied in the middle of API and async function
-      // node does not know whether our sending data is XML/JSON/Text.
-      // express.json() -> inbuilt middleware in express -> which confirms our sending data in JSON and converts it into JS object
-      // API data -> express.json()(JSON->JS Object) -> async function uses the JS Object and produces the result.
-  // app.post("/movies", express.json(), async function (request, response) {
-  const data = request.body;
-  console.log(data);
-  // db.movies.insertMany(data)
-  const result = await client.db("recap").collection("movies").insertMany(data);
-
-  response.send(result);
-});
-
-app.delete("/movies/:id",async function (request, response) { 
-  const {id} = request.params; 
-  
-  // db.movies.deleteOne({id:'100'}) -> db query
-  const result = await client
-  .db("recap")
-  .collection("movies")
-  .deleteOne({id: id});
-
-  console.log(result);
-  result.deletedCount 
-  ? response.send({message:"movie deleted successfully"}) 
-  : response.status(404).send({message: "movie not found"});
-
-}); 
-
-app.put("/movies/:id",async function (request, response) { 
-  const {id} = request.params; 
-  const data = request.body;
-  // db.movies.updateOne({id:'100'},{$set:{rating:9}}) -> db query
-  const result = await client
-  .db("recap")
-  .collection("movies")
-  .updateOne({id: id},{$set:data});
-
-  console.log(result);
-  response.send(result); 
-  
-});
+app.use("/movies",moviesRouter); 
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
 
+export {client};
---
+++ routes/movies.route.js
@@ -0,0 +1,87 @@
+import express from "express";
+const router = express.Router();
+import {client} from "../index.js";
+
+  router.get("/", async function (request, response) {
+    // request.query returns string value.
+    // In our movies collection rating is in number. So if request.query is rating means convert them into number.
+    if(request.query.rating){
+      request.query.rating = +request.query.rating;
+    };
+    console.log(request.query);
+    // db.movies.find({})
+  
+    // cursor - pagination when you search in google, it will show top 10 results only. if you want means you go to next next page.
+    // why they don't show all the million results??? why because when you get all the millions of data, browser will strucks to produce results. Also it affects browser database too.
+    // To avoid this browser shows top 10 reults. Likewise mongoDB also shows first 20 results. 
+    
+    // Cursor - pagination | Cursor -> Array | toArray()
+    // const movies = await client.db("recap").collection("movies").find({});
+    // const movies = await client.db("recap").collection("movies").find({}).toArray();
+    
+    // query params: in google or any other search bar, when you seach an item you will can notice (For eg: http link + `?search_query=ps2`). here we are filtering an item.
+    // const movies = await client.db("recap").collection("movies").find({name:"RRR"}).toArray();
+    const movies = await client.db("recap").collection("movies").find(request.query).toArray();
+    // console.log(movies);
+    response.send(movies);
+  });
+  
+  router.get("/:id",async function (request, response) { 
+    const {id} = request.params; 
+    // console.log(request.params, id); 
+    // const movie = movies.find((mv) => mv.id == id); // using local data
+    // getting data from database
+    // db.movies.findOne({id:'100'}) -> db query
+    const movie = await client.db("recap").collection("movies").findOne({id: id});
+  
+    console.log(movie);
+   
+    movie ? response.send(movie) : response.status(404).send({message: "movie not found"});
+  
+  }); 
+   
+  router.post("/", async function (request, response) {
+    // middleware -> applied in the middle of API and async function
+        // node does not know whether our sending data is XML/JSON/Text.
+        // express.json() -> inbuilt middleware in express -> which confirms our sending data in JSON and converts it into JS object
+        // API data -> express.json()(JSON->JS Object) -> async function uses the JS Object and produces the result.
+    // app.post("/movies", express.json(), async function (request, response) {
+    const data = request.body;
+    console.log(data);
+    // db.movies.insertMany(data)
+    const result = await client.db("recap").collection("movies").insertMany(data);
+  
+    response.send(result);
+  });
+  
+  router.delete("/:id",async function (request, response) { 
+    const {id} = request.params; 
+    
+    // db.movies.deleteOne({id:'100'}) -> db query
+    const result = await client
+    .db("recap")
+    .collection("movies")
+    .deleteOne({id: id});
+  
+    console.log(result);
+    result.deletedCount 
+    ? response.send({message:"movie deleted successfully"}) 
+    : response.status(404).send({message: "movie not found"});
+  
+  }); 
+  
+  router.put("/:id",async function (request, response) { 
+    const {id} = request.params; 
+    const data = request.body;
+    // db.movies.updateOne({id:'100'},{$set:{rating:9}}) -> db query
+    const result = await client
+    .db("recap")
+    .collection("movies")
+    .updateOne({id: id},{$set:data});
+  
+    console.log(result);
+    response.send(result); 
+    
+  });
+
+export default router;
