f073b0784515f89e32424ef00770c5d818b9e0de
karthigayani <karthi1826@gmail.com>
2023-10-12 12:46:53 +0530

    Converted token as middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,5 +5,6 @@ export const auth = (request, response, next) => {     //step:26 export auth
     const token = request.header("x-auth-token"); // step:25 extracting token. token is in header (header is in request )
     console.log("token", token); // step:27
     // step:28 refer movies.route.js
+    // step:29 refer movies.route.js
     next(); // step:30 calling next. // Purpose of next => It is used to call the next function
 };
