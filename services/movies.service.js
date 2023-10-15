092ee897368ffaf94e61fd538decf50b29e2a93c
karthigayani <karthi1826@gmail.com>
2023-10-16 02:40:13 +0530

    id to _id

--- services/movies.service.js
+++ services/movies.service.js
@@ -1,22 +1,23 @@
 import { client } from "../index.js";
-
+import { ObjectId } from "mongodb"; // importing ObjectId
 export async function updateMovieById(id, data) {
   return await client
     .db("recap")
     .collection("movies")
-    .updateOne({ id: id }, { $set: data });
+    .updateOne({ _id: new ObjectId(id) }, { $set: data });
 }
 export async function deleteMovieById(id) {
   return await client
     .db("recap")
     .collection("movies")
-    .deleteOne({ id: id });
+    .deleteOne({ _id: new ObjectId(id) });
 }
 export async function createMovies(data) {
   return await client.db("recap").collection("movies").insertMany(data);
 }
 export async function getMovieById(id) {
-  return await client.db("recap").collection("movies").findOne({ id: id });
+  // console.log("Objectid", id);
+  return await client.db("recap").collection("movies").findOne({ _id: new ObjectId(id) });
 }
 export async function getMovies(request) {
   return await client.db("recap").collection("movies").find(request.query).toArray();
